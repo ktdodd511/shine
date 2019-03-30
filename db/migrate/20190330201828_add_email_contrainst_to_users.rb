4054175626a8f8dd7bb2e7c8f97935d07f982d80
katiedodd <ktdodd511@gmail.com>
2019-03-30 16:38:15 -0400

    added migration to prevent users from using incorrect email

---
+++ db/migrate/20190330201828_add_email_contrainst_to_users.rb
@@ -0,0 +1,20 @@
+class AddEmailContrainstToUsers < ActiveRecord::Migration[5.1]
+  def up
+    execute %{
+      ALTER TABLE
+        users
+      ADD CONSTRAINT
+        email_must_be_company_email
+      CHECK ( email ~* '^[^@]+@gojel\\.com' )
+    }
+  end
+
+  def down
+    execute %{
+    ALTER TABLE
+      users
+    DROP CONSTRAINT
+      email_must_be_company_email
+  }
+  end
+end
